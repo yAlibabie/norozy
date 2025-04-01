348b22f8c6d537547ee740bfbfd8a32e59aeafd0
Y.Alibabaie <yasamin.alibabaie.1081@gmail.com>
2025-04-01 11:57:38 +0330

    EntityNotFoundException is made in the exception package

--- norozy/src/db/exeption/EntityNotFoundException.java
+++ norozy/src/db/exeption/EntityNotFoundException.java
@@ -1,7 +1,17 @@
 package db.exeption;
 
-public class EntityNotFoundException extends RuntimeException {
-  public EntityNotFoundException(String message) {
-    super(message);
-  }
+public class EntityNotFoundException extends Exception {
+
+
+    public EntityNotFoundException() {
+        System.out.println("Cannot find entity");
+    }
+
+    public EntityNotFoundException(String message) {
+        System.out.println(message);
+    }
+
+    public EntityNotFoundException(int id) {
+        System.out.println("Cannot find entity with id: " + id);
+    }
 }
