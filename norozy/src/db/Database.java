3f406bc67329db9b1962bbca004ff3eef8f6fc57
Y.Alibabaie <yasamin.alibabaie.1081@gmail.com>
2025-04-01 11:48:43 +0330

    Database class

--- norozy/src/db/Database.java
+++ norozy/src/db/Database.java
@@ -1,4 +1,42 @@
 package db;
 
+import java.util.ArrayList;
+import db.exeption.EntityNotFoundException;
+
+
 public class Database {
+
+    private static ArrayList<Entity> entities = new ArrayList<>();
+    private static int nextId = 1;
+
+    private Database () {}
+
+    public static void add(Entity e) {
+        e.setId(nextId++);
+        entities.add(e);
+    }
+
+    public static Entity get (int id) throws EntityNotFoundException {
+        for (Entity entity : entities) {
+            if(entity.getId() == id) {
+                return entity;
+            }
+        }
+        throw new EntityNotFoundException(id);
+    }
+
+    public static void delete ( int id) throws EntityNotFoundException {
+        Entity entity = get(id);
+        entities.remove(entity);
+    }
+
+    public static Entity update (Entity e) throws EntityNotFoundException {
+        for (Entity entity : entities) {
+            if (entity.getId() == e.id) {
+                entity = e;
+                return entity;
+            }
+        }
+        throw new EntityNotFoundException(e.id);
+    }
 }
