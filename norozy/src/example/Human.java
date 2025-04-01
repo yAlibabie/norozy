79edc64d3a67f46ecc150c8d43cda13b50293859
Y.Alibabaie <yasamin.alibabaie.1081@gmail.com>
2025-04-01 11:58:30 +0330

    Human is made in the example package

--- norozy/src/example/Human.java
+++ norozy/src/example/Human.java
@@ -1,4 +1,11 @@
 package example;
 
-public class Human {
+import db.Entity;
+
+public class Human extends Entity {
+    public String name;
+
+    public Human (String name) {
+        this.name = name;
+    }
 }
