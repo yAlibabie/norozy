cb64b509105de1917239af408af50c7f6a8d7d04
Y.Alibabaie <yasamin.alibabaie.1081@gmail.com>
2025-04-11 16:41:41 +0330

    Trackable class was writen

--- norozy/src/db/Trackable.java
+++ norozy/src/db/Trackable.java
@@ -1,4 +1,13 @@
 package db;
 
+import java.util.Date;
+
 public interface Trackable {
+
+    public void setCreationDate(Date date);
+    public Date getCreationDate();
+
+    public void setLastModificationDate(Date date);
+    public Date getLastModificationDate();
+
 }
