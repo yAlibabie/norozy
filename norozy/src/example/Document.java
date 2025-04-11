81ee93b978d0b3db57aaa88afff750c340f51293
Y.Alibabaie <yasamin.alibabaie.1081@gmail.com>
2025-04-11 16:44:31 +0330

    document in example package was writen

--- norozy/src/example/Document.java
+++ norozy/src/example/Document.java
@@ -1,4 +1,53 @@
 package example;
 
-public class Document {
+import db.Entity;
+import db.Trackable;
+import java.util.Date;
+
+public class Document extends Entity implements Trackable {
+    public String content;
+    public Date creationDate ;
+    public Date lastModificationDate;
+    public static final int DOCUMENT_ENTITY_CODE = 1;
+
+
+    public Document (String content) {
+        this.content = content;
+    }
+
+    @Override
+    public int getEntityCode () {
+        return DOCUMENT_ENTITY_CODE;
+    }
+
+    @Override
+    public Document copy () {
+        Document copy = new Document(this.content);
+        copy.id = this.id;
+        copy.creationDate = this.creationDate;
+        copy.lastModificationDate = this.lastModificationDate;
+
+        return copy;
+    }
+
+    @Override
+    public void setCreationDate(Date date) {
+        this.creationDate = date;
+    }
+
+    @Override
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    @Override
+    public void setLastModificationDate(Date date){
+        this.lastModificationDate = date;
+    }
+
+    @Override
+    public Date getLastModificationDate() {
+        return lastModificationDate;
+    }
+
 }
